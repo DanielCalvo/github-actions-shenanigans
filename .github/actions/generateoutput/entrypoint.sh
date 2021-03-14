66c0779907a0ef3370dbfc55cd33157cc5694865
DanielCalvo <vioseven@gmail.com>
2021-03-14 18:02:25 +0100

    Pushing docker workflow

--- .github/actions/generateoutput/entrypoint.sh
+++ .github/actions/generateoutput/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-echo "So I'm just echoing things from the generate output action"
-echo "I want this output to be multiline!"
-echo "So here's a third line"
+echo "::set-output name=lines::So I'm just echoing things from the generate output action"
+echo "::set-output name=lines::I want this output to be multiline!"
+echo "::set-output name=lines::So here's a third line"
